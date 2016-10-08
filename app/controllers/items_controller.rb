3463ade7a5265edc4037ee275e35df5a65cc0078
橋本渉 <w0129akjm@gmail.com>
2016-10-08 19:34:41 +0900

    Make controllers.about my page ,edit.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,32 @@ class ItemsController < ApplicationController
   def create
     @item=Item.create(item_params)
   end
+  def edit
+    @item = Item.find(params[:id]) 
+  end
+  def update
+    item=Item.find(params[:id])
+    if item.user_id == current_user.id
+        item.update(item_params)
+    end
+  end
+  def show
+      @item=  Item.find(params[:id])
+      comment=@item.comments.includes(:user)
+  end
+  def destroy
+    item=Item.find(params[:id])
+    if item.user_id = current_user.id
+      item.destroy 
+    end 
+  end
+
 
 private
 def item_params
   params.permit(:image,:description)
 end
+def move_to_index
+    redirect_to action: :index unless user_signed_in? 
+  end
 end
